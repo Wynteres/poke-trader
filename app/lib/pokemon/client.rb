a1ecdc811a67c316f4a1bf3e0a812ea062a5cab1
Raul Coan <raul.coan@locaweb.com.br>
2020-11-16 13:52:59 -0300

    fix Pokemon namespace

--- app/lib/pokemon/client.rb
+++ app/lib/pokemon/client.rb
@@ -1,50 +1,48 @@
 # frozen_string_literal: true
 
-class Pokemon
-  class Client
-    POKE_API_URL = ENV['POKE_API_URL']
-    QUANTITY_PER_PAGE = 22
+class Pokemon::Client
+  POKE_API_URL = ENV['POKE_API_URL']
+  QUANTITY_PER_PAGE = 22
 
-    class PokemonNotFound < StandardError; end
+  class PokemonNotFound < StandardError; end
 
-    def self.list(**args)
-      new.list(args)
-    end
+  def self.list(**args)
+    new.list(args)
+  end
 
-    def list(page_index:)
-      offset = (page_index - 1) * QUANTITY_PER_PAGE
+  def list(page_index:)
+    offset = (page_index - 1) * QUANTITY_PER_PAGE
 
-      response = HTTParty.get("#{POKE_API_URL}?offset=#{offset}&limit=#{QUANTITY_PER_PAGE}")
+    response = HTTParty.get("#{POKE_API_URL}?offset=#{offset}&limit=#{QUANTITY_PER_PAGE}")
 
-      {
-        next_page: parse_page_number(response.fetch('next')),
-        previous_page: parse_page_number(response.fetch('previous')),
-        pokemons: response.fetch('results')
-      }
-    end
+    {
+      next_page: parse_page_number(response.fetch('next')),
+      previous_page: parse_page_number(response.fetch('previous')),
+      pokemons: response.fetch('results')
+    }
+  end
 
-    def self.fetch(**args)
-      new.fetch(args)
-    end
+  def self.fetch(**args)
+    new.fetch(args)
+  end
 
-    def fetch(id:)
-      response = HTTParty.get("#{POKE_API_URL}/#{id}")
+  def fetch(id:)
+    response = HTTParty.get("#{POKE_API_URL}/#{id}")
 
-      raise PokemonNotFound unless response.success?
+    raise PokemonNotFound unless response.success?
 
-      response
-    end
+    response
+  end
 
-    private
+  private
 
-    def parse_page_number(url)
-      return if url.nil?
+  def parse_page_number(url)
+    return if url.nil?
 
-      uri = URI.parse(url)
-      query_params = CGI.parse(uri.query)
-      offset = query_params['offset'].first.to_i
+    uri = URI.parse(url)
+    query_params = CGI.parse(uri.query)
+    offset = query_params['offset'].first.to_i
 
-      (offset / QUANTITY_PER_PAGE) + 1
-    end
+    (offset / QUANTITY_PER_PAGE) + 1
   end
 end
