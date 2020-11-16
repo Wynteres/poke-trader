b7f0db60f2ebeef77d8e5a6f0f4ab0507d925344
Raul Coan <raul.coan@locaweb.com.br>
2020-11-16 12:28:52 -0300

    add tests to pokemon client

--- app/lib/pokemon/client.rb
+++ app/lib/pokemon/client.rb
@@ -2,18 +2,21 @@ class Pokemon::Client
   POKE_API_URL = ENV['POKE_API_URL']
   QUANTITY_PER_PAGE = 22
 
+  class PokemonNotFound < StandardError; end
+
   def self.list(**args)
     new.list(args)
   end
 
   def list(page_index:)
     offset = (page_index - 1) * QUANTITY_PER_PAGE
+
     response = HTTParty.get("#{POKE_API_URL}?offset=#{offset}&limit=#{QUANTITY_PER_PAGE}")
 
     {
-      next_page: parse_page_number(response['next']),
-      previous_page: parse_page_number(response['previous']),
-      pokemons: response['results']
+      next_page: parse_page_number(response.fetch('next')),
+      previous_page: parse_page_number(response.fetch('previous')),
+      pokemons: response.fetch('results')
     }
   end
 
@@ -22,7 +25,11 @@ class Pokemon::Client
   end
 
   def fetch(id:)
-    HTTParty.get("#{POKE_API_URL}/#{id}")
+    response = HTTParty.get("#{POKE_API_URL}/#{id}")
+
+    raise PokemonNotFound unless response.success?
+
+    response
   end
 
   private
---
+++ spec/lib/pokemon/client_spec.rb
@@ -0,0 +1,103 @@
+require 'rails_helper'
+
+RSpec.describe Pokemon::Client, :vcr do
+  describe '#list' do
+    describe 'when page index exists' do
+      it 'returns a hash with pokemon and page info' do
+        params = { page_index: 1 }
+        result = described_class.list(params)
+
+        expect(result.keys)
+          .to contain_exactly(:next_page, :previous_page, :pokemons)
+      end
+
+      it 'returns a list with the quantity per page number of pokemons' do
+        params = { page_index: 1 }
+        result = described_class.list(params)
+
+        expect(result[:pokemons].size)
+          .to be(Pokemon::Client::QUANTITY_PER_PAGE)
+      end
+    end
+
+    describe 'when page index does not exists' do
+      it 'returns a hash with pokemon and page info' do
+        params = { page_index: 0 }
+        result = described_class.list(params)
+
+        expect(result.keys)
+          .to contain_exactly(:next_page, :previous_page, :pokemons)
+      end
+
+      it 'returns a empty list of pokemons ' do
+        params = { page_index: 0 }
+        result = described_class.list(params)
+
+        expect(result[:pokemons]).to be_empty
+      end
+    end
+
+    describe 'when poke api url is missing' do
+      it 'raises error TypeError' do
+        stub_const("#{described_class}::QUANTITY_PER_PAGE", '')
+        params = { page_index: 1 }
+
+        expect { described_class.list(params) }.to raise_error(TypeError)
+      end
+    end
+
+    describe 'when poke api is out of service' do
+      it 'raises error' do
+        WebMock.stub_request(:any, /pokeapi.co/)
+               .to_return(status: [500, 'Internal Server Error'])
+
+        params = { page_index: 1 }
+
+        expect { described_class.list(params) }.to raise_error()
+      end
+    end
+  end
+
+  describe '#fetch' do
+    describe 'when pokemon id exists' do
+      it 'returns a hash with expected attributes' do
+        params = { id: 1 }
+
+        response = described_class.fetch(params)
+
+        expect(response.keys).to include('name', 'base_experience', 'sprites')
+        expect(response['sprites'].keys).to include('front_default')
+      end
+    end
+
+    describe 'when pokemon id does not exists' do
+      it 'returns a' do
+        params = { id: 0 }
+
+        expect { described_class.fetch(params) }
+          .to raise_error(Pokemon::Client::PokemonNotFound)
+
+      end
+    end
+
+    describe 'when poke api url is missing' do
+      it 'raises error TypeError' do
+        stub_const("#{described_class}::QUANTITY_PER_PAGE", '')
+        params = { page_index: 1 }
+
+        expect { described_class.list(params) }.to raise_error(TypeError)
+      end
+    end
+
+    describe 'when poke api is out of service' do
+      it 'raises error' do
+        WebMock.stub_request(:any, /pokeapi.co/)
+               .to_return(status: [500, 'Internal Server Error'])
+
+        params = { page_index: 1 }
+
+        expect { described_class.list(params) }.to raise_error(KeyError)
+      end
+    end
+  end
+end
