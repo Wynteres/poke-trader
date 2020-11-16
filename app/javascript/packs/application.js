8ee2efd3d37bfa9ef44164335db5500bf1764791
Raul Coan <raul.coan@locaweb.com.br>
2020-11-16 08:28:55 -0300

    add favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import "bootstrap"
+const images = require.context('../images', true)
+
 import axios from "axios";
 
 import ReactOnRails from 'react-on-rails';
